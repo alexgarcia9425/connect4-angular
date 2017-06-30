cb093f2f05a0c255a77374f5c2805e3df4362396
Alex Garcia <alexgarcia9425@gmail.com>
2017-06-29 22:26:26 -0500

    Change connect4-game.service.js endpoint to heroku's

--- app/connect4/connect4-game.service.js
+++ app/connect4/connect4-game.service.js
@@ -2,7 +2,7 @@ angular.
   module('connect4').
   factory('GamesService', ['$resource',
     function($resource) {
-      return $resource('../../connect4-server/games/the_only_one.php', {}, {
+      return $resource('https://safe-mesa-68625.herokuapp.com/games/the_only_one.php', {}, {
         getGameStatus: {
           method: 'GET'
         },
